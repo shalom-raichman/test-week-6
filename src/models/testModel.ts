68ab3547ab805383aebd88b5a326993ac7ccf11f
Shalom Raichman <shalomr1770@gmail.com>
2024-10-13 12:08:15 +0300

    adding test schema

--- src/models/testModel.ts
+++ src/models/testModel.ts
@@ -1,5 +1,5 @@
 import mongoose, { Schema, Document, Types } from "mongoose"
-import { IUser } from "./userModel"
+import userModel, { IUser } from "./userModel"
 import { IClass } from "./classModel"
 
 
@@ -10,6 +10,27 @@ export interface ITest extends Document {
     class_id: Types.ObjectId | IClass
 }
 
-const TestSchema = new Schema<ITest>({})
+const TestSchema = new Schema<ITest>({
+    test_name: {
+      type: String,
+      minlength: [3, "username is to short!"],
+      maxlength: [30, "username is to long!"],
+      required: [true, "username must be provided"],
+      unique: true
+    },
+    score: {
+        type: Number
+    },
+    student_id: {
+        type: Types.ObjectId,
+        ref: "User",
+        required: [true, "student id must be provided"]
+    },
+    class_id: {
+        type: Types.ObjectId,
+        ref: "Class",
+        required: [true, "class id mus be provided"]
+    }
+  })
 
 export default mongoose.model<ITest>("Test", TestSchema)
