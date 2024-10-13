8d8b575bb99496cc027103d11bbf1866a741ecc6
Shalom Raichman <shalomr1770@gmail.com>
2024-10-13 12:15:33 +0300

    added class schema

--- src/models/classModel.ts
+++ src/models/classModel.ts
@@ -9,6 +9,30 @@ export interface IClass extends Document {
     tests_id: Types.ObjectId[] | ITest[]
 }
 
-const ClassSchema = new Schema<IClass>({})
+const ClassSchema = new Schema<IClass>({
+    class_name: {
+        type: String,
+        minlength: [3, "username is to short!"],
+        maxlength: [30, "username is to long!"],
+        required: [true, "username must be provided"],
+        unique: true
+      },
+    teacher_id: {
+        type: Types.ObjectId,
+        ref: "User",
+        required: [true, "class must heve a teacher! teachcer id must be provided"],
+        unique: true,
+    },
+    students_id: {
+        type: [Types.ObjectId],
+        ref: "User",
+        default: []
+    },
+    tests_id: {
+        type: [Types.ObjectId],
+        ref: "Test",
+        default: []
+    }
+})
 
 export default mongoose.model<IClass>("Class", ClassSchema)
