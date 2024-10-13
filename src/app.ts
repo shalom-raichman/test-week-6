ce0b8833175e024a1d9e6e6a585a5706207e5f60
Shalom Raichman <shalomr1770@gmail.com>
2024-10-13 14:41:35 +0300

    update swagger link

--- src/app.ts
+++ src/app.ts
@@ -28,7 +28,7 @@ app.use("/api/class", classRouter)
 app.use(errorHandler)
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`)
+  console.log(`Server is running on port  http://localhost:${PORT}/api-docs`)
 })
 
 export default app
